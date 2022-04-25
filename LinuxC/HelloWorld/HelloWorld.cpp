e74203be80177edeed33201540801d2a79cdc091
Waiting-For-Wind <www_739886387@163.com>
2022-04-25 09:46:21 +0800

    1

--- LinuxC/HelloWorld/HelloWorld.cpp
+++ LinuxC/HelloWorld/HelloWorld.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
 using namespace std;
-int main(void)
+#define put(helloworld) #helloworld
+int main(int argc,char *argv[])
 {
+    cout<<put(helloworld)<<endl;
 
-	cout << "HelloWorld" << endl;
-    int sum=0;
-    int a=0;
-    int b=0;
-    cout<<"Plase input first number:"<<endl;
-    cin>>a;
-    cout<<"Plase input second number:"<<endl;
-    cin>>b;
-    cout<<"The sum is:"<<a+b<<endl;
-	return 0;
-
-    
+    return 0;
 }
